9cda891f39bd239f0c25e46c5106599b13ccdd6c
Frank Anneveld <frank@anneveld.it>
2022-09-12 20:42:43 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {
+  FlexibleConnectedPositionStrategy,
+  OverlayPositionBuilder
+} from '@angular/cdk/overlay';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'material-test';
+export class AppComponent implements OnInit {
+  public isOpen = false;
+  public positionStrategy!: FlexibleConnectedPositionStrategy;
+
+  @ViewChild('button', { static: true })
+  public button!: ElementRef<HTMLButtonElement>;
+
+  constructor(private overlayPositionBuilder: OverlayPositionBuilder) {}
+
+  ngOnInit(): void {
+    this.positionStrategy = this.overlayPositionBuilder
+      .flexibleConnectedTo(this.button)
+      .withPositions([
+        {
+          originX: 'end',
+          originY: 'bottom',
+          overlayX: 'start',
+          overlayY: 'top',
+        },
+      ]);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { TestComponent } from './test/test.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    OverlayModule
   ],
   providers: [],
   bootstrap: [AppComponent]
